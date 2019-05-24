149790fb2ae7b2f8841ae3eaca4b18ca1edc0ba3
zlcting <zlcting@gmail.com>
2019-05-24 15:31:53 +0800

    first commit

---
+++ SpiderKeeper/config.py
@@ -0,0 +1,49 @@
+# Statement for enabling the development environment
+import os
+
+DEBUG = True
+
+# Define the application directory
+
+
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(os.path.abspath('.'), 'SpiderKeeper.db')
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+DATABASE_CONNECT_OPTIONS = {}
+
+# Application threads. A common general assumption is
+# using 2 per available processor cores - to handle
+# incoming requests using one and performing background
+# operations using the other.
+THREADS_PER_PAGE = 2
+
+# Enable protection agains *Cross-site Request Forgery (CSRF)*
+CSRF_ENABLED = True
+
+# Use a secure, unique and absolutely secret key for
+# signing the data.
+CSRF_SESSION_KEY = "secret"
+
+# Secret key for signing cookies
+SECRET_KEY = "secret"
+
+# log
+LOG_LEVEL = 'INFO'
+
+# spider services
+SERVER_TYPE = 'scrapyd'
+SERVERS = ['http://localhost:6800']
+
+# basic auth
+NO_AUTH = False
+BASIC_AUTH_USERNAME = 'admin'
+BASIC_AUTH_PASSWORD = 'admin'
+BASIC_AUTH_FORCE = True
+
+
+#以下为集成常量
+
+LOGIN_PROJECTKEY = 'c5b0358039877dbb7d280590e99435df'
+COOKIE_PREFIX = 'newhousespider_'
+COOKIE_KEY = 'Hwn6EUA56v'
